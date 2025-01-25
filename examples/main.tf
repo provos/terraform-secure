1d8f849d40334f5e95d5b783f35e3f78aa41c38a
provos <provos@gmail.com>
2025-01-25 13:50:42 -0800

    example tf that is secure and then we make it insecure

---
+++ examples/main.tf
@@ -0,0 +1,57 @@
+provider "google" {
+  project = "fit-accumulator-440500-e5"
+  region  = "us-central1"
+}
+
+resource "google_compute_network" "default" {
+  name                    = "default-network"
+  auto_create_subnetworks = true
+}
+
+resource "google_compute_firewall" "allow-http-https" {
+  name    = "allow-http-https"
+  network = google_compute_network.default.self_link
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443"]
+  }
+
+  source_ranges = ["10.0.0.0/8"]
+  target_tags   = ["http-server"]
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "example-vm"
+  machine_type = "e2-micro"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.default.name
+
+    access_config {
+      # Required to allow access from the internet
+    }
+  }
+
+  metadata = {
+    enable-oslogin = "TRUE"
+  }
+
+  tags = ["http-server"]
+}
+
+resource "google_compute_address" "external_ip" {
+  name   = "vm-external-ip"
+  region = "us-central1"
+}
+
+output "instance_external_ip" {
+  value = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
+}
